96db55fae4e249a9ba186076bfc08eaa0dd93562
SAED ALLOUZI <saedallouzi@Samers-MacBook-Pro.local>
2021-05-20 00:22:56 +0300

    homework

--- app.js
+++ app.js
@@ -10,19 +10,35 @@
 // https://www.w3schools.com/js/js_dates.asp
 // https://www.digitalocean.com/community/tutorials/understanding-date-and-time-in-javascript
 // HINT: recycling code is never bad practice.
-
 // This functions should check the integrity of the parameters and pass true/false
 function checkParamsFn(year, month, day) {
-  // Write your code here
+  if (!Number.isInteger(year)) return false;
+  else if (!Number.isInteger(month)) return false;
+  else if (!Number.isInteger(day)) return false;
+  else return true;
 }
+console.log(checkParamsFn(1998, 7, 5));
 
 // This functions checks if the person is or above 18 years of age, return true/false
 function checkOverEighteenFn(year, month, day) {
-  // Write your code here
+  let yearx = new Date();
+  let monthx = new Date();
+  let dayx = new Date();
+  if (yearx.getFullYear() < year + 18) return false;
+  else if (yearx.getFullYear() > year + 18) return true;
+  else if (monthx.getMonth() < month) return false;
+  else if (monthx.getMonth() > month) return true;
+  else if (dayx.getDate() > day) return false;
+  else if (dayx.getDate() <= day) return true;
 }
+console.log(checkOverEighteenFn(2003, 5, 19));
+
 
 function calculateAgeFn(year, month, day) {
-  // Write your code here
+  if (!checkParamsFn) return "error " ;
+  else if (!checkOverEighteenFn) return "error :"
+  else {
+}
 }
 
 // Look at the naming of the functions. it looks like salwaBaqer, where
